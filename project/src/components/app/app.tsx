d73ff91ff463f72152e153294d58c0b5147c833e
shaulov <shineginn@gmail.com>
2022-07-14 13:58:05 +0300

    Add routes

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,13 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
 import MainScreen from '../../pages/main-screen/main-screen';
+import FilmScreen from '../../pages/film-screen/film-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import MyListScreen from '../../pages/my-list-screen/my-list-screen';
+import PlayerScreen from '../../pages/player-screen/player-screen';
+import ReviewScreen from '../../pages/review-screen/review-screen';
+import Screen404 from '../../pages/404-screen/404-screen';
 
 const randomFilm = {
   name: 'The Grand Budapest Hotel',
@@ -10,7 +19,39 @@ function App(): JSX.Element {
   const { name, genre, releaseDate } = randomFilm;
 
   return (
-    <MainScreen name={name} genre={genre} releaseDate={releaseDate}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainScreen name={name} genre={genre} releaseDate={releaseDate} />}
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<FilmScreen />}
+        >
+          <Route
+            path={AppRoute.Review}
+            element={<ReviewScreen />}
+          />
+        </Route>
+        <Route
+          path={AppRoute.Player}
+          element={<PlayerScreen />}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={<MyListScreen />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginScreen />}
+        />
+        <Route
+          path="*"
+          element={<Screen404 />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/const.tsx
@@ -0,0 +1,8 @@
+export enum AppRoute {
+  Login = '/login',
+  Film = '/films/:id',
+  Root = '/',
+  MyList = '/mylist',
+  Player = '/player/:id',
+  Review = 'review',
+}
