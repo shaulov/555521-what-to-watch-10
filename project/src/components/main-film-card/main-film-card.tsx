4d6b0bbd9f78fc5b70f42f9f809607855a4c19b2
shaulov <shineginn@gmail.com>
2022-07-18 13:24:54 +0300

    Create component for header

--- project/src/components/main-film-card/main-film-card.tsx
+++ project/src/components/main-film-card/main-film-card.tsx
@@ -1,3 +1,5 @@
+import PageHeader from '../page-header/page-header';
+
 import { Film } from '../../types/film';
 
 type MainFilmCardProps = {
@@ -13,26 +15,7 @@ function MainFilmCard({film}: MainFilmCardProps) : JSX.Element {
 
       <h1 className="visually-hidden">WTW</h1>
 
-      <header className="page-header film-card__head">
-        <div className="logo">
-          <a className="logo__link">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
-
-        <ul className="user-block">
-          <li className="user-block__item">
-            <div className="user-block__avatar">
-              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-            </div>
-          </li>
-          <li className="user-block__item">
-            <a className="user-block__link">Sign out</a>
-          </li>
-        </ul>
-      </header>
+      <PageHeader />
 
       <div className="film-card__wrap">
         <div className="film-card__info">
