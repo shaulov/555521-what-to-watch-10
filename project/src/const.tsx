ccdb656b76750626b601eb6022581da360494a4e
shaulov <shineginn@gmail.com>
2022-07-18 16:05:12 +0300

    Add tuples for months

--- project/src/const.tsx
+++ project/src/const.tsx
@@ -12,3 +12,5 @@ export enum AuthorizationStatus {
   NoAuth = 'NO_AUTH',
   Unknown = 'UNKNOWN',
 }
+
+export const MONTH: readonly string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
