07a890c56aea8c1f6f91f97af6cd000f592cc790
shaulov <shineginn@gmail.com>
2022-07-26 20:07:36 +0300

    Fix mentor remarks

--- project/src/components/film-overview/film-overview.tsx
+++ project/src/components/film-overview/film-overview.tsx
@@ -2,6 +2,8 @@ import { Film } from '../../types/film';
 
 import { SHORT_STAIRS_NUMBER } from '../../const';
 
+import { getFilmRatingLevel } from '../../utils/getFilmRatingLevel';
+
 type FilmOverviewProps = {
   film: Film;
 }
@@ -10,26 +12,12 @@ function FilmOverview({film}: FilmOverviewProps) : JSX.Element {
 
   const { rating, ratingsNumber, description, director, starring } = film;
 
-  let filmRatingLevel: string;
-
-  if (rating < 3) {
-    filmRatingLevel = 'Bad';
-  } else if (rating >= 3 && rating < 5) {
-    filmRatingLevel = 'Normal';
-  } else if (rating >= 5 && rating < 8) {
-    filmRatingLevel = 'Good';
-  } else if (rating >= 8 && rating < 10) {
-    filmRatingLevel = 'Very good';
-  } else {
-    filmRatingLevel = 'Awesome';
-  }
-
   return(
     <>
       <div className="film-rating">
         <div className="film-rating__score">{rating}</div>
         <p className="film-rating__meta">
-          <span className="film-rating__level">{filmRatingLevel}</span>
+          <span className="film-rating__level">{getFilmRatingLevel(rating)}</span>
           <span className="film-rating__count">{`${ratingsNumber} ratings`}</span>
         </p>
       </div>
---
+++ project/src/utils/getFilmRatingLevel.tsx
@@ -0,0 +1,13 @@
+export const getFilmRatingLevel = (rating: number): string => {
+  if (rating < 3) {
+    return 'Bad';
+  } else if (rating >= 3 && rating < 5) {
+    return 'Normal';
+  } else if (rating >= 5 && rating < 8) {
+    return 'Good';
+  } else if (rating >= 8 && rating < 10) {
+    return 'Very good';
+  } else {
+    return 'Awesome';
+  }
+};
