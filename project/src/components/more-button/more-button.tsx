614c9830bfa63acc7b974a63cb7c7d37e06a11ab
shaulov <shineginn@gmail.com>
2022-08-02 16:16:48 +0300

    Create more button component

---
+++ project/src/components/more-button/more-button.tsx
@@ -0,0 +1,9 @@
+function MoreButton(): JSX.Element {
+  return (
+    <div className="catalog__more">
+      <button className="catalog__button" type="button">Show more</button>
+    </div>
+  );
+}
+
+export default MoreButton;
