e1a40e41c43fa05daa8741fada79040b077d2d7b
shaulov <shineginn@gmail.com>
2022-08-02 17:19:40 +0300

    Add action and reducer for show more button

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,14 +1,15 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { changeGenre, getFilmList } from './action';
+import { changeGenre, getFilmList, showMoreFilms } from './action';
 import { films } from '../mocks/films';
 import { reviews } from '../mocks/reviews';
 
-import { DEFAULT_GENRE } from '../const';
+import { DEFAULT_GENRE, FILMS_PER_STEP_COUNT } from '../const';
 
 const initialState = {
   genre: DEFAULT_GENRE,
   films,
   reviews,
+  filmsPerStep: FILMS_PER_STEP_COUNT,
 };
 
 export const reducer = createReducer(initialState, (builder) => {
@@ -18,5 +19,8 @@ export const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(getFilmList, (state) => {
       state.films = state.genre === DEFAULT_GENRE ? films : films.filter((film) => film.genre === state.genre);
+    })
+    .addCase(showMoreFilms, (state, action) => {
+      state.filmsPerStep = action.payload;
     });
 });
