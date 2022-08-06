69ed85cb5c6e153437d14620b2f8d183c87ce484
shaulov <shineginn@gmail.com>
2022-08-06 17:59:40 +0300

    Update login screen

--- project/src/pages/login-screen/login-screen.tsx
+++ project/src/pages/login-screen/login-screen.tsx
@@ -1,50 +1,92 @@
+import { useRef, FormEvent } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAppSelector, useAppDispatch } from '../../hooks';
+import { loginAction } from '../../store/api-actions';
+import { AuthData } from '../../types/auth-data';
+
+import { AppRoute, AuthorizationStatus } from '../../const';
+
+import Logo from '../../components/logo/logo';
+
 function LoginScreen (): JSX.Element {
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
+
+  const loginRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const dispatch = useAppDispatch();
+
+  const onSubmit = (authData: AuthData) => {
+    dispatch(loginAction(authData));
+  };
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (loginRef.current !== null && passwordRef.current !== null) {
+      onSubmit({
+        login: loginRef.current.value,
+        password: passwordRef.current.value,
+      });
+    }
+  };
+
   return (
-    <div className="user-page">
-      <header className="page-header user-page__head">
-        <div className="logo">
-          <a href="main.html" className="logo__link">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
+    <>
+      {
+        authorizationStatus === AuthorizationStatus.Auth && (<Navigate to={AppRoute.Root} replace />)
+      }
+      <div className="user-page">
+        <header className="page-header user-page__head">
+          <Logo light={false} />
+
+          <h1 className="page-title user-page__title">Sign in</h1>
+        </header>
+
+        <div className="sign-in user-page__content">
+          <form
+            action=""
+            className="sign-in__form"
+            onSubmit={handleSubmit}
+          >
+            <div className="sign-in__fields">
+              <div className="sign-in__field">
+                <input
+                  ref={loginRef}
+                  className="sign-in__input"
+                  type="email" placeholder="Email address"
+                  name="user-email"
+                  id="user-email"
+                />
+                <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
+              </div>
+              <div className="sign-in__field">
+                <input
+                  ref={passwordRef}
+                  className="sign-in__input"
+                  type="password"
+                  placeholder="Password"
+                  name="user-password"
+                  id="user-password"
+                />
+                <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
+              </div>
+            </div>
+            <div className="sign-in__submit">
+              <button className="sign-in__btn" type="submit">Sign in</button>
+            </div>
+          </form>
         </div>
 
-        <h1 className="page-title user-page__title">Sign in</h1>
-      </header>
+        <footer className="page-footer">
+          <Logo light />
 
-      <div className="sign-in user-page__content">
-        <form action="#" className="sign-in__form">
-          <div className="sign-in__fields">
-            <div className="sign-in__field">
-              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" />
-              <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
-            </div>
-            <div className="sign-in__field">
-              <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
-              <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
-            </div>
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
           </div>
-          <div className="sign-in__submit">
-            <button className="sign-in__btn" type="submit">Sign in</button>
-          </div>
-        </form>
+        </footer>
       </div>
-
-      <footer className="page-footer">
-        <div className="logo">
-          <a href="main.html" className="logo__link logo__link--light">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
-
-        <div className="copyright">
-          <p>© 2019 What to watch Ltd.</p>
-        </div>
-      </footer>
-    </div>
+    </>
   );
 }
 
