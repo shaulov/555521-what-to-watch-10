24744b88998476ec11c890eeb807b5fb2b789114
shaulov <shineginn@gmail.com>
2022-08-13 13:34:54 +0300

    Fix mentor remarks

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -38,6 +38,8 @@ export const createApi = (): AxiosInstance => {
       if (error.response && shouldDisplayError(error.response)) {
         toast.warn(error.response.data.error);
       }
+
+      throw error;
     }
   );
 
